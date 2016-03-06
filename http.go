495c287ef77c8b765b6ace9b218f00baae32afd3
Phelan Vendeville <phelan.vendeville@gmail.com>
2016-03-05 18:31:04 -0800

    Add logging to the http server.
    - also add a notes file
    - next steps would be more advanced logging, and some way to export the stats
    aside from logging them, so that they can be consumed by some graphing solution.
    - see:
    https://rcrowley.org/talks/strange-loop-2013.html#16
    https://www.reddit.com/r/golang/comments/439jb4/is_there_a_library_that_can_measure_latency_of/

--- http.go
+++ http.go
@@ -2,21 +2,52 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 )
 
+type WrapHTTPHandler struct {
+	m http.Handler
+}
 
-// handler takes in an http.Responsewriting and an http.Request .
-// the ResponseWriter is an interface type and can contain either a copy of a pointer (reference-type) or a copy of a value (value-type).
-// An http.ResponseWriter value assembles the HTTP server's response; by writing to it, we send data to the HTTP client.
-// the Request is a reference, a data structure that represents the client HTTP request. 
-// The trailing [1:] means "create a sub-slice of Path from the 1st character to the end," dropping the leading "/" from the path name.
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s! The method is %s.\n", r.URL.Path[1:], r.Method)
-    // send POST requests with $ curl -X POST localhost:8080/something
+func (h *WrapHTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	lw := &loggedResponse{ResponseWriter: w, status: 200}
+	start := time.Now()
+	h.m.ServeHTTP(lw, r)
+	elapsed := time.Since(start)
+	log.SetPrefix("[Info]")
+	log.Printf("[%s] %s - %d, time elapsed was: %dns.\n", 
+		r.RemoteAddr, r.URL, lw.status, elapsed)
+}
+
+type loggedResponse struct {
+	http.ResponseWriter
+	status int
+}
+
+func (l *loggedResponse) WriteHeader(status int) {
+	l.status = status
+	l.ResponseWriter.WriteHeader(status)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	// The "/" pattern matches everything, so we need to check
+    // that we're at the root here.
+    if r.URL.Path != "/" {
+            http.NotFound(w, r)
+            return
+    }
+    fmt.Fprintf(w, "Welcome to the home page!")
+}
+
+func goodbye(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "goodbye")
 }
 
 func main() {
-    http.HandleFunc("/", handler) // handle all requests to the web root ("/") with handler function
-    http.ListenAndServe(":8080", nil) // listen on port 8080 on any interface (":8080"), blocking until terminated
+	http.HandleFunc("/", hello)
+	http.HandleFunc("/goodbye", goodbye)
+	http.Handle("/hello", http.RedirectHandler("/", http.StatusFound))
+	log.Fatalln(http.ListenAndServe(":8080", &WrapHTTPHandler{http.DefaultServeMux}))
 }
