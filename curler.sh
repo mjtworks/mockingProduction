81303bf65b42bce27cb6b5c9e964a87a47dd6b23
Phelan Vendeville <phelan.vendeville@gmail.com>
2016-03-27 13:25:59 -0700

    update port in the curler

--- curler.sh
+++ curler.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # randomly issue different curl commands
-requests=("curl -X GET localhost:8080/" "curl -X POST localhost:8080/" "curl -X GET localhost:8080/fail" "curl -X GET localhost:8080/redirect_me")
+requests=("curl -X GET localhost:7070/" "curl -X POST localhost:7070/" "curl -X GET localhost:7070/fail" "curl -X GET localhost:7070/redirect_me")
 seed=$$$(date +%s)
 
 for i in `seq 1 500`
