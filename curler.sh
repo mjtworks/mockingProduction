1e468068d23855af58efc2f3c5a2ea7b2276b155
Phelan Vendeville <phelan.vendeville@gmail.com>
2016-03-27 14:55:42 -0700

    Make the curler run forever

--- curler.sh
+++ curler.sh
@@ -2,9 +2,10 @@
 # randomly issue different curl commands
 requests=("curl -X GET localhost:7070/" "curl -X POST localhost:7070/" "curl -X GET localhost:7070/fail" "curl -X GET localhost:7070/redirect_me")
 
-for i in `seq 1 500`
+while [ 1 ]
 do
   selectedexpression=${requests[$RANDOM % ${#requests[@]} ]}
   `$selectedexpression` >/dev/null 2>&1 
+  # sleep $[ ( $RANDOM % 10 )  + 1 ]s
 done
 
