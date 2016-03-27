f11748763ff27872785c0a033dfc72b26a6d5592
Phelan Vendeville <phelan.vendeville@gmail.com>
2016-03-27 13:39:23 -0700

    update curler

--- curler.sh
+++ curler.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # randomly issue different curl commands
 requests=("curl -X GET localhost:7070/" "curl -X POST localhost:7070/" "curl -X GET localhost:7070/fail" "curl -X GET localhost:7070/redirect_me")
-seed=$$$(date +%s)
 
 for i in `seq 1 500`
 do
-  selectedexpression=${requests[$seed % ${#requests[@]} ]}
-  `$selectedexpression`
+  selectedexpression=${requests[$RANDOM % ${#requests[@]} ]}
+  `$selectedexpression` >/dev/null 2>&1 
 done
+
