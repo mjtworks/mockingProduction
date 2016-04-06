3264370a196eccf764e34a1c54573b18fa096d04
Phelan Vendeville <phelan.vendeville@gmail.com>
2016-04-05 22:06:27 -0700

    Finish grafana setup
    - add random time jitter to curler script
    - update grafana notes and include config file
    - add screenshot of the working dashboard

--- curler.sh
+++ curler.sh
@@ -6,6 +6,6 @@ while [ 1 ]
 do
   selectedexpression=${requests[$RANDOM % ${#requests[@]} ]}
   `$selectedexpression` >/dev/null 2>&1 
-  # sleep $[ ( $RANDOM % 10 )  + 1 ]s
+  sleep $[ ( $RANDOM % 5 )  + 1 ]s
 done
 
