41acc00f060f1051c3d9249fefb0833641ab679d
Phelan Vendeville <phelan.vendeville@gmail.com>
2016-04-11 19:07:31 -0700

    add all commands to get going to the startup script

--- startup.sh
+++ startup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # start prometheus on localhost:9090/ [metrics, graphs]
-./prometheus -config.file=/home/phelan/Repositories/mockingProduction/prometheus/prometheus.yml -storage.local.path=/tmp/data
+/opt/prometheus-0.17.0.linux-386/prometheus -config.file=/home/phelan/Repositories/mockingProduction/prometheus/prometheus.yml -storage.local.path=/tmp/data &&
 
 # start grafana on localhost:3000
-sudo service grafana-server start
+sudo service grafana-server start &&
 
 # start http server
-./http_server -port=7070
+http_server/http_server -port=7070 &&
+
+# start curler
+hammers/curler.sh
 
